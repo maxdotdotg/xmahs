c457e0600b6d1afece64ac42ea4df3f630b370fd
Max G <maxdotdotg@gmail.com>
2020-01-05 15:36:30 -0500

    add nested loop for gifts, clean up gifts function

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,26 @@
 
 fn main() {
     println!("days of xmas!");
+
+    // increment loop for days
     let mut days = 1;
     while days < 13 {
-        println!("on day {} of xmas, somebody gave to me {}", days, gifts(days));
+        println!("on day {} of xmas, somebody gave to me...", days);
+
+        // decrement loop for gifts
+        let mut gift_counter = days;
+        while gift_counter >= 0 {
+            println!("{}", gifts(gift_counter));
+            gift_counter -= 1;
+        }
         days += 1;
     }
 }
 
 fn gifts(x: i32) -> &'static str {
+    // static because str value is unknown at runtime/can change?
+    // https://doc.rust-lang.org/rust-by-example/scope/lifetime/static_lifetime.html
+    // compiler, yo 
     let gift = match x {
         12 => "12 drummers drumming,",
         11 => "11 pipers piping,",
@@ -25,8 +37,9 @@ fn gifts(x: i32) -> &'static str {
         5 => "FIVE! GOLDEN! RINGS!",
         4 => "4 calling birds,",
         3 => "3 french hens,",
-        2 => "2 turtle doves,",
-        _ => "a partridge in a pear tree",
+        2 => "2 turtle doves, and",
+        1 => "a partridge in a pear tree",
+        _ => "",
     };
     return gift;
 }
